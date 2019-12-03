fc76e4839901050cf257806c188674b76397c10d
Tomáš Mlynarič <mlynarict@gmail.com>
2019-12-03 13:34:25 +0100

    Snackbar message SingleLiveEvent

--- mobile/src/main/kotlin/com/strv/mlyko/kitchensink/ui/main/MainFragment.kt
+++ mobile/src/main/kotlin/com/strv/mlyko/kitchensink/ui/main/MainFragment.kt
@@ -1,9 +1,13 @@
 package com.strv.mlyko.kitchensink.ui.main
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.strv.mlyko.kitchensink.arch.BaseFragmentWithViewModel
 import com.strv.mlyko.kitchensink.databinding.FragmentMainBinding
 
@@ -18,6 +22,14 @@ class MainFragment : BaseFragmentWithViewModel<MainViewModel, FragmentMainBindin
 
 	override fun onNextClick() {
 		findNavController().navigate(MainFragmentDirections.actionMainFragmentToAuthFragment())
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		viewModel.displayMessage.observe(viewLifecycleOwner) {
+			val message = it ?: return@observe
+			Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+		}
 	}
 }
 
--- mobile/src/main/kotlin/com/strv/mlyko/kitchensink/ui/main/MainViewModel.kt
+++ mobile/src/main/kotlin/com/strv/mlyko/kitchensink/ui/main/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.strv.mlyko.kitchensink.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.strv.mlyko.kitchensink.arch.events.SingleLiveEvent
 
 private const val KEY_STATE_COUNTER = "KEY_STATE_COUNTER"
 
@@ -9,14 +11,18 @@ class MainViewModel(
 	private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-	val counter = savedStateHandle.getLiveData(KEY_STATE_COUNTER, 0)
+	val displayMessage = SingleLiveEvent<String>()
 
-	override fun onCleared() {
-		super.onCleared()
-		savedStateHandle.set(KEY_STATE_COUNTER, counter.value ?: 0)
+	val counter = MutableLiveData<Int>(savedStateHandle.get(KEY_STATE_COUNTER) ?: 0)
+
+	init {
+		counter.observeForever {
+			savedStateHandle.set(KEY_STATE_COUNTER, it)
+		}
 	}
 
 	fun onPlusClick() {
 		counter.value = (counter.value ?: 0) + 1
+		displayMessage.value = "New counter value is ${counter.value}"
 	}
 }

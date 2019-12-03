b5ca64f3bc3511c1352302797879f623893bce65
Tomáš Mlynarič <mlynarict@gmail.com>
2019-12-03 09:51:36 +0100

    Add viewmodel

--- mobile/src/main/kotlin/com/strv/mlyko/kitchensink/ui/main/MainFragment.kt
+++ mobile/src/main/kotlin/com/strv/mlyko/kitchensink/ui/main/MainFragment.kt
@@ -1,7 +1,14 @@
 package com.strv.mlyko.kitchensink.ui.main
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import com.strv.mlyko.kitchensink.R
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+	val viewModel: MainViewModel by viewModels()
+}
+
+class MainViewModel : ViewModel() {
+
 }
